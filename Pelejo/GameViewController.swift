fae0333e0900f43b3aeacdadce966e17ae5e658e
Luis Pereira <lfbp@Luiss-MacBook-Pro.local>
2020-06-26 03:06:23 -0300

    jump, move left and right doen; no animation yet but there are some sprites loaded

--- Pelejo/GameViewController.swift
+++ Pelejo/GameViewController.swift
@@ -11,42 +11,55 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
- 
+    
+    var scene = GKScene()
+    var sceneNode = GameScene()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
-            
-            // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = false
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler))
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler))
+        let tap = UITapGestureRecognizer(target: self, action: #selector(tapHandler))
+        rightSwipe.direction = .right
+        leftSwipe.direction = .left
+        view.addGestureRecognizer(rightSwipe)
+        view.addGestureRecognizer(leftSwipe)
+        view.addGestureRecognizer(tap)
+        
+        scene = GKScene(fileNamed: "GameScene")!
+        // Get the SKScene from the loaded GKScene
+        sceneNode = (scene.rootNode as! GameScene?)!
+        // Copy gameplay related content over to the scene
+        sceneNode.entities = scene.entities
+        sceneNode.graphs = scene.graphs
+        // Set the scale mode to scale to fit the window
+        sceneNode.scaleMode = .aspectFill
+        
+        // Present the scene
+        if let view = self.view as! SKView? {
+            view.presentScene(sceneNode)
+            view.ignoresSiblingOrder = false
+            view.showsFPS = true
+            view.showsNodeCount = true
         }
+        
     }
-
+    
+    @objc func swipeHandler(sender:UISwipeGestureRecognizer){
+        sceneNode.moveFabiano(swipe: sender)
+    }
+    
+    @objc func tapHandler(sender:UITapGestureRecognizer){
+        sceneNode.jumpFabiano()
+    }
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -54,7 +67,7 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
---
+++ Pelejo/MovimentClass.swift
@@ -0,0 +1,32 @@
+//
+//  MovimentClass.swift
+//  Pelejo
+//
+//  Created by Luis Pereira on 26/06/20.
+//  Copyright © 2020 Elaine  Cruz. All rights reserved.
+//
+
+import SpriteKit
+
+class MovimentClass{
+    
+    public var xMovimentVariation:CGFloat = 50
+    public var yJumpVariation:CGFloat = 400
+    public var interval:TimeInterval = 0.5
+    
+    func moveLeft(body:SKNode){
+        body.run(SKAction.move(by: CGVector(dx: -xMovimentVariation, dy: 0), duration: interval), withKey: "Left")
+    }
+    
+    func moveRight(body:SKNode){
+        body.run(SKAction.move(by: CGVector(dx: xMovimentVariation, dy: 0), duration: interval), withKey: "Right")
+    }
+    
+    func stopMovimentForKey(body:SKNode, movimentKey:String){
+        body.removeAction(forKey: movimentKey)
+    }
+    
+    func jumpMoviment(body:SKNode){
+        body.physicsBody?.applyImpulse(CGVector(dx: 0.0, dy: yJumpVariation))
+    }
+}
