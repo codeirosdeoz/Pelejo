9a1ac6cbd0ec1930e4859dcb45935eea4f47aebd
Luis Felipe <lfbp@academy.cin.ufpe.br>
2020-06-29 21:57:38 -0300

    one but at a time

--- Pelejo/GameViewController.swift
+++ Pelejo/GameViewController.swift
@@ -18,8 +18,6 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
-        // including entities and graphs.
         let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler))
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeHandler))
         let tap = UITapGestureRecognizer(target: self, action: #selector(tapHandler))
@@ -30,42 +28,13 @@ class GameViewController: UIViewController {
         view.addGestureRecognizer(tap)
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
+            let scene = SKScene(fileNamed: "GameScene")!
+            sceneNode = scene as! GameScene
+            sceneNode.scaleMode = .aspectFill
+            view.presentScene(sceneNode)
             view.ignoresSiblingOrder = false
-            
-            view.showsFPS = false
-            view.showsNodeCount = false
         }
         
-        //scene = GKScene(fileNamed: "GameScene")!
-        // Get the SKScene from the loaded GKScene
-        //sceneNode = (scene.rootNode as! GameScene?)!
-        // Copy gameplay related content over to the scene
-        //sceneNode.entities = scene.entities
-        //sceneNode.graphs = scene.graphs
-        // Set the scale mode to scale to fit the window
-        //sceneNode.scaleMode = .aspectFill
-        
-        // Present the scene
-        //if let view = self.view as! SKView? {
-            //view.presentScene(sceneNode)
-            //view.ignoresSiblingOrder = false
-            //view.showsFPS = true
-            //view.showsNodeCount = true
-        //}
-        
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        print("cancel")
     }
     
     @objc func swipeHandler(sender:UISwipeGestureRecognizer){
